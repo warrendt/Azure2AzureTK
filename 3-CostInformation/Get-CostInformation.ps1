dc3076363848a26bc9b85d6170473eec598362b7
Donovan Hughes <dohughes@microsoft.com>
2025-06-19 18:25:18 +0200

    Add error checking and Excel output

--- 3-CostInformation/Get-CostInformation.ps1
+++ 3-CostInformation/Get-CostInformation.ps1
@@ -3,7 +3,9 @@
     Take a collection of given resource IDs and return the cost incurred during previous months,
     grouped as needed. For this we use the Microsoft.CostManagement provider of each subscription.
     Requires Az.CostManagement module 0.4.2 or later.
+    Requires ImportExcel module if Excel output is requested.
     PS1> Install-Module -Name Az.CostManagement
+    PS1> Install-Module -Name ImportExcel
 
 .PARAMETER startDate
     The start date of the period to be examined (default is the first day of the previous month)
@@ -34,7 +36,7 @@ param (
     [string]$endDate      = (Get-Date).AddDays(-1 * (Get-Date).Day).ToString("yyyy-MM-dd"),     # the last day of the previous month
     [string]$resourceFile = "resources.json",
     [string]$outputFile   = "resource_cost",
-    [string]$outputFormat = "json",                # json, csv or console
+    [string]$outputFormat = "json",                # json, csv, excel or console
     [switch]$testMode
 )
 
@@ -46,22 +48,25 @@ if (-not (Test-Path -Path $resourceFile)) {
 }
 
 # Check that the requested output format is valid
-if ($outputFormat -notin @("json", "csv", "console")) {
-    Write-Error "Output format '$outputFormat' is not supported. Supported formats are 'json', 'csv', and 'console'."
+if ($outputFormat -notin @("json", "csv", "excel", "console")) {
+    Write-Error "Output format '$outputFormat' is not supported. Supported formats are 'json', 'csv', 'excel', and 'console'."
+    exit 1
+}
+
+# Check if the needed modules are installed 
+if (-not (Get-Module -ListAvailable -Name Az.CostManagement)) {
+    Write-Error "Az.CostManagement module is not installed. Please install it using 'Install-Module -Name Az.CostManagement'."
+    exit 1
+}
+if ($outputFormat -eq "excel" -and -not (Get-Module -ListAvailable -Name ImportExcel)) {
+    Write-Error "ImportExcel module is not installed. Please install it using 'Install-Module -Name ImportExcel'."
     exit 1
 }
 
 # Timeframe
-# Supported types are BillingMonthToDate, Custom, MonthToDate, TheLastBillingMonth, TheLastMonth, WeekToDate
 $timeframe = "Custom"
 
-# Granularity
-# Supported types are Daily and Monthly so far. Omit just to get the total cost.
-#$granularity = "Monthly"
-
 # Type
-# Supported types are Usage (deprecated), ActualCost, and AmortizedCost
-# https://stackoverflow.com/questions/68223909/in-the-azure-consumption-usage-details-api-what-is-the-difference-between-the-m
 $type = "AmortizedCost"
 
 # Read the content of the workloads file
@@ -103,7 +108,6 @@ $grouping = @(
 )
 
 # Aggregation
-# Supported types are Sum, Average, Minimum, Maximum, Count, and Total.
 $aggregation = @{
     PreTaxCost = @{
         type = "Sum"
@@ -149,24 +153,39 @@ for ($subIndex = 0; $subIndex -lt $subscriptionIds.Count; $subIndex++) {
         }
         $table += $row
     }
-    # For testing - limit to one subscription
-    # $subIndex = $subscriptionIds.Count
+    
+    # If test mode is enabled, break after the first subscription
+    if ($testMode) {
+        Write-Output "Test mode enabled, stopping after the first subscription."
+        $subIndex = $subscriptionIds.Count
+    }
 }
 
-# Output in the desired format
-if ($outputFormat -eq "json") {
-    if ($outputFile -notmatch '\.json$') {
-        $outputFile += ".json"
+switch ($outputFormat) {
+    "json" {
+        if ($outputFile -notmatch '\.json$') {
+            $outputFile += ".json"
+        }
+        $table | ConvertTo-Json | Out-File -FilePath $outputFile -Encoding UTF8
+        Write-Output "$($table.Count) rows written to $outputFile"
     }
-    $table | ConvertTo-Json | Out-File -FilePath $outputFile -Encoding UTF8
-    Write-Output "$($table.Count) rows written to $outputFile"
-} elseif ($outputFormat -eq "csv") {
-    if ($outputFile -notmatch '\.csv$') {
-        $outputFile += ".csv"
+    "csv" {
+        if ($outputFile -notmatch '\.csv$') {
+            $outputFile += ".csv"
+        }
+        $table | Export-Csv -Path $outputFile -NoTypeInformation -Encoding UTF8
+        Write-Output "$($table.Count) rows written to $outputFile"
     }
-    $table | Export-Csv -Path $outputFile -NoTypeInformation -Encoding UTF8
-    Write-Output "$($table.Count) rows written to $outputFile"
-} else {
-    # Display the table in the console
-    $table | Format-Table -AutoSize
+    "excel" {
+        if ($outputFile -notmatch '\.xlsx$') {
+            $outputFile += ".xlsx"
+        }
+        $label = "CostInformation"
+        $table | Export-Excel -WorksheetName $label -TableName $label -Path .\$outputFile
+        Write-Output "$($table.Count) rows written to $outputFile"
+    }
+    Default {
+        # Display the table in the console
+        $table | Format-Table -AutoSize
+    }
 }
